e6bb6150fba9062ad0595c31797e68ede941db9a
MaciejSpalek <spwrtt@gmail.com>
2025-04-02 19:18:03 +0200

    Created typeDefs and mongo model

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const {ApolloServer } = require('apollo-server');
-const mongoose = require('mongoose');
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
 
-const MONGODB = '';
+const MONGODB =
+  "mongodb+srv://cryptospwrtt:H760skRq4wyIZqrY@cluster0.ewdop6y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+
+const server = ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
+  console.log("MongoDB connected");
+  return server.listern({ port: 5000 });
+}).then(res => {
+  console.log(`Server running at ${res.url}`)
+});
